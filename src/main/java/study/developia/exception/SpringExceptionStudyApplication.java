ba1b98f92c971eeafa4ede24dad8045f4e8c994a
developiaa <developiaa@gmail.com>
2021-11-27 22:11:39 +0900

    first commit

---
+++ src/main/java/study/developia/exception/SpringExceptionStudyApplication.java
@@ -0,0 +1,13 @@
+package study.developia.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExceptionStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExceptionStudyApplication.class, args);
+    }
+
+}
