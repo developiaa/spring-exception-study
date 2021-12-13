046e88ba7639481c52fbd6aa7a96c65b7fcfbb4f
developiaa <developiaa@gmail.com>
2021-12-13 23:32:04 +0900

    feat : Add handler exception resolver

--- src/main/java/study/developia/exception/api/ApiExceptionController.java
+++ src/main/java/study/developia/exception/api/ApiExceptionController.java
@@ -13,8 +13,12 @@ public class ApiExceptionController {
 
     @GetMapping("/api/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
-        if(id.equals("ex")){
+        if (id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
+        }
+
+        if (id.equals("bad")) {
+            throw new IllegalArgumentException("잘못된 입력 값");
         }
 
         return new MemberDto(id, "hello " + id);
@@ -22,7 +26,7 @@ public class ApiExceptionController {
 
     @Data
     @AllArgsConstructor
-    static class MemberDto{
+    static class MemberDto {
         private String memberId;
         private String name;
     }
