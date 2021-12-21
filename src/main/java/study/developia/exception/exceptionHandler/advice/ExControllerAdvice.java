aecd337811a06ec96df231cbe0ad38640f42acbd
developiaa <developiaa@gmail.com>
2021-12-21 23:34:26 +0900

    refactor : Add RestControllerAdvice

---
+++ src/main/java/study/developia/exception/exceptionHandler/advice/ExControllerAdvice.java
@@ -0,0 +1,35 @@
+package study.developia.exception.exceptionHandler.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import study.developia.exception.exception.UserException;
+import study.developia.exception.exceptionHandler.ErrorResult;
+
+@Slf4j
+@RestControllerAdvice(basePackages = "study.developia.exception")
+public class ExControllerAdvice {
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("BAD", e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> userExHandler(UserException e) {
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ErrorResult exHandler(Exception e) {
+        log.error("[exceptionHandler] ex", e);
+        return new ErrorResult("EX", "내부 오류");
+    }
+
+}
